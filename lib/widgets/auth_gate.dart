72c27e94186b11d0f5e48f62271ecedad4ba02c3
Casey Rogers <caseycrogers@berkeley.edu>
2022-08-04 12:35:04 -0700

    broke firestore, added connectivity check

--- lib/widgets/auth_gate.dart
+++ lib/widgets/auth_gate.dart
@@ -2,6 +2,8 @@ import 'package:cast_me_app/business_logic/clients/auth_manager.dart';
 import 'package:cast_me_app/pages/sign_in_page_view.dart';
 import 'package:cast_me_app/util/adaptive_material.dart';
 
+import 'package:connectivity_plus/connectivity_plus.dart';
+
 import 'package:flutter/material.dart';
 
 class CastMeAuthGate extends StatelessWidget {
@@ -15,23 +17,46 @@ class CastMeAuthGate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final AuthManager authManager = AuthManager.instance;
-    return AnimatedBuilder(
-      animation: authManager,
-      builder: (context, child) {
-        if (authManager.isLoading) {
-          return const AdaptiveMaterial(
-            adaptiveColor: AdaptiveColor.background,
-            child: Center(
-              child: CircularProgressIndicator(),
-            ),
+    return StreamBuilder<ConnectivityResult>(
+        stream: Connectivity().onConnectivityChanged,
+        builder: (context, connectivityResult) {
+          print(connectivityResult.data);
+          if (connectivityResult.data == ConnectivityResult.none) {
+            return const AdaptiveMaterial(
+              adaptiveColor: AdaptiveColor.surface,
+              child: Center(
+                child: AdaptiveText(
+                  'You are offline!\n'
+                  'CastMe is currently online only.',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            );
+          }
+          return AnimatedBuilder(
+            animation: authManager,
+            builder: (context, child) {
+              if (authManager.isLoading) {
+                return const AdaptiveMaterial(
+                  adaptiveColor: AdaptiveColor.background,
+                  child: Center(
+                    child: SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: CircularProgressIndicator(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                );
+              }
+              if (!authManager.isFullySignedIn) {
+                return const SignInPageView();
+              }
+              return child!;
+            },
+            child: child,
           );
-        }
-        if (!authManager.isFullySignedIn) {
-          return const SignInPageView();
-        }
-        return child!;
-      },
-      child: child,
-    );
+        });
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import audio_session
 import cloud_firestore
+import connectivity_plus_macos
 import desktop_webview_auth
 import firebase_auth
 import firebase_core
@@ -22,6 +23,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
