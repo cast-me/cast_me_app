3082be7b087cc3a1dbbd3b836dfd6aa9e04c30e5
Casey Rogers <caseycrogers@berkeley.edu>
2022-08-05 13:37:55 -0700

    renamed user to profile

---
+++ lib/business_logic/models/protobufs/cast_me_profile_base.pb.dart
@@ -0,0 +1,100 @@
+///
+//  Generated code. Do not modify.
+//  source: lib/business_logic/models/protobufs/cast_me_profile_base.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,constant_identifier_names,directives_ordering,library_prefixes,non_constant_identifier_names,prefer_final_fields,return_of_invalid_type,unnecessary_const,unnecessary_import,unnecessary_this,unused_import,unused_shown_name
+
+import 'dart:core' as $core;
+
+import 'package:protobuf/protobuf.dart' as $pb;
+
+class CastMeProfileBase extends $pb.GeneratedMessage {
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'CastMeProfileBase', package: const $pb.PackageName(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'cast_me_app'), createEmptyInstance: create)
+    ..aOS(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'id')
+    ..aOS(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'userName')
+    ..aOS(3, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'displayName')
+    ..aOS(4, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'profilePictureUri')
+    ..hasRequiredFields = false
+  ;
+
+  CastMeProfileBase._() : super();
+  factory CastMeProfileBase({
+    $core.String? id,
+    $core.String? userName,
+    $core.String? displayName,
+    $core.String? profilePictureUri,
+  }) {
+    final _result = create();
+    if (id != null) {
+      _result.id = id;
+    }
+    if (userName != null) {
+      _result.userName = userName;
+    }
+    if (displayName != null) {
+      _result.displayName = displayName;
+    }
+    if (profilePictureUri != null) {
+      _result.profilePictureUri = profilePictureUri;
+    }
+    return _result;
+  }
+  factory CastMeProfileBase.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory CastMeProfileBase.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  CastMeProfileBase clone() => CastMeProfileBase()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  CastMeProfileBase copyWith(void Function(CastMeProfileBase) updates) => super.copyWith((message) => updates(message as CastMeProfileBase)) as CastMeProfileBase; // ignore: deprecated_member_use
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static CastMeProfileBase create() => CastMeProfileBase._();
+  CastMeProfileBase createEmptyInstance() => create();
+  static $pb.PbList<CastMeProfileBase> createRepeated() => $pb.PbList<CastMeProfileBase>();
+  @$core.pragma('dart2js:noInline')
+  static CastMeProfileBase getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<CastMeProfileBase>(create);
+  static CastMeProfileBase? _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.String get id => $_getSZ(0);
+  @$pb.TagNumber(1)
+  set id($core.String v) { $_setString(0, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasId() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearId() => clearField(1);
+
+  @$pb.TagNumber(2)
+  $core.String get userName => $_getSZ(1);
+  @$pb.TagNumber(2)
+  set userName($core.String v) { $_setString(1, v); }
+  @$pb.TagNumber(2)
+  $core.bool hasUserName() => $_has(1);
+  @$pb.TagNumber(2)
+  void clearUserName() => clearField(2);
+
+  @$pb.TagNumber(3)
+  $core.String get displayName => $_getSZ(2);
+  @$pb.TagNumber(3)
+  set displayName($core.String v) { $_setString(2, v); }
+  @$pb.TagNumber(3)
+  $core.bool hasDisplayName() => $_has(2);
+  @$pb.TagNumber(3)
+  void clearDisplayName() => clearField(3);
+
+  @$pb.TagNumber(4)
+  $core.String get profilePictureUri => $_getSZ(3);
+  @$pb.TagNumber(4)
+  set profilePictureUri($core.String v) { $_setString(3, v); }
+  @$pb.TagNumber(4)
+  $core.bool hasProfilePictureUri() => $_has(3);
+  @$pb.TagNumber(4)
+  void clearProfilePictureUri() => clearField(4);
+}
+
---
+++ lib/business_logic/models/protobufs/cast_me_profile_base.pbjson.dart
@@ -0,0 +1,23 @@
+///
+//  Generated code. Do not modify.
+//  source: lib/business_logic/models/protobufs/cast_me_profile_base.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,constant_identifier_names,deprecated_member_use_from_same_package,directives_ordering,library_prefixes,non_constant_identifier_names,prefer_final_fields,return_of_invalid_type,unnecessary_const,unnecessary_import,unnecessary_this,unused_import,unused_shown_name
+
+import 'dart:core' as $core;
+import 'dart:convert' as $convert;
+import 'dart:typed_data' as $typed_data;
+@$core.Deprecated('Use castMeProfileBaseDescriptor instead')
+const CastMeProfileBase$json = const {
+  '1': 'CastMeProfileBase',
+  '2': const [
+    const {'1': 'id', '3': 1, '4': 1, '5': 9, '10': 'id'},
+    const {'1': 'user_name', '3': 2, '4': 1, '5': 9, '10': 'userName'},
+    const {'1': 'display_name', '3': 3, '4': 1, '5': 9, '10': 'displayName'},
+    const {'1': 'profile_picture_uri', '3': 4, '4': 1, '5': 9, '10': 'profilePictureUri'},
+  ],
+};
+
+/// Descriptor for `CastMeProfileBase`. Decode as a `google.protobuf.DescriptorProto`.
+final $typed_data.Uint8List castMeProfileBaseDescriptor = $convert.base64Decode('ChFDYXN0TWVQcm9maWxlQmFzZRIOCgJpZBgBIAEoCVICaWQSGwoJdXNlcl9uYW1lGAIgASgJUgh1c2VyTmFtZRIhCgxkaXNwbGF5X25hbWUYAyABKAlSC2Rpc3BsYXlOYW1lEi4KE3Byb2ZpbGVfcGljdHVyZV91cmkYBCABKAlSEXByb2ZpbGVQaWN0dXJlVXJp');
