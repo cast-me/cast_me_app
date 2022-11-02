6165a1a234ef33e4f6ae9aa96f0745b1f17f60a9
Casey Rogers <caseycrogers@berkeley.edu>
2022-11-02 14:12:06 -0700

    added link posting logic

---
+++ lib/widgets/post_page/external_link_field.dart
@@ -0,0 +1,35 @@
+import 'package:cast_me_app/util/uri_utils.dart';
+import 'package:flutter/material.dart';
+
+class ExternalLinkField extends StatelessWidget {
+  const ExternalLinkField({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  final TextEditingController controller;
+
+  static bool isValid(String url) {
+    if (url.isEmpty) {
+      return true;
+    }
+    return UriUtils.tryParse(url, schemes: ['http', 'https']) != null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, _) {
+        return TextField(
+          controller: controller,
+          decoration: InputDecoration(
+            errorText: isValid(controller.text)
+                ? null
+                : 'Link must be a valid url including \'http\' or \'https\'',
+          ),
+        );
+      },
+    );
+  }
+}
