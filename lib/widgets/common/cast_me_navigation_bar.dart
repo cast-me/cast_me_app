e9384f9dd19ea630c796396d67093e8aa1ff5327
Casey Rogers <caseycrogers@berkeley.edu>
2022-11-24 14:29:44 -0800

    hide nav bar on keyboard show

--- lib/widgets/common/cast_me_navigation_bar.dart
+++ lib/widgets/common/cast_me_navigation_bar.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 // Project imports:
 import 'package:cast_me_app/business_logic/cast_me_bloc.dart';
 import 'package:cast_me_app/business_logic/models/cast_me_tab.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 
 class CastMeNavigationBar extends StatelessWidget {
   const CastMeNavigationBar({
@@ -15,26 +16,36 @@ class CastMeNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return NavigationBarTheme(
-      data: NavigationBarThemeData(
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        indicatorColor: Colors.black54,
-        labelTextStyle: MaterialStateProperty.all(
-          Theme.of(context).textTheme.bodyText1,
-        ),
-      ),
-      child: MediaQuery(
-        data: MediaQuery.of(context)
-            .removeViewInsets(removeTop: true)
-            .removePadding(removeTop: true),
-        child: NavigationBar(
-          selectedIndex: tab.index,
-          onDestinationSelected: (index) {
-            CastMeBloc.instance.onTabIndexChanged(index);
-          },
-          destinations: CastMeTabs.tabs.values.toList(),
-        ),
-      ),
+    return KeyboardVisibilityBuilder(
+      builder: (context, isVisible) {
+        if (isVisible) {
+          // Don't show the nav bar if the keyboard is visible.
+          return Container();
+        }
+        return NavigationBarTheme(
+          data: NavigationBarThemeData(
+            backgroundColor: Theme.of(context).colorScheme.surface,
+            indicatorColor: Colors.black54,
+            labelTextStyle: MaterialStateProperty.all(
+              Theme.of(context).textTheme.bodyText1,
+            ),
+          ),
+          child: MediaQuery(
+            // NavigationBar wraps itself in `SafeArea` which accidentally applied
+            // top padding nonsense. Stop it from doing so.
+            data: MediaQuery.of(context)
+                .removeViewInsets(removeTop: true)
+                .removePadding(removeTop: true),
+            child: NavigationBar(
+              selectedIndex: tab.index,
+              onDestinationSelected: (index) {
+                CastMeBloc.instance.onTabIndexChanged(index);
+              },
+              destinations: CastMeTabs.tabs.values.toList(),
+            ),
+          ),
+        );
+      }
     );
   }
 }
