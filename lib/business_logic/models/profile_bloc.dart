285d0b3cf801c8ce3777a23acca47c0f3b736969
Casey Rogers <caseycrogers@berkeley.edu>
2023-02-22 20:36:23 -0500

    deleted unused import

--- lib/business_logic/models/profile_bloc.dart
+++ lib/business_logic/models/profile_bloc.dart
@@ -1,9 +1,6 @@
 // Flutter imports:
 import 'package:flutter/foundation.dart';
 
-// Package imports:
-import 'package:supabase_flutter/supabase_flutter.dart';
-
 // Project imports:
 import 'package:cast_me_app/business_logic/clients/auth_manager.dart';
 import 'package:cast_me_app/business_logic/models/profile_form_data.dart';
